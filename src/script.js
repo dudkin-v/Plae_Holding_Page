3a7ad2aae492d005dfe85c2d023d030bd1b63f5e
dudkin-v <dudkin31v@gmail.com>
2022-02-24 16:09:53 +0200

    rewrite functional for slider (replace id to index)

--- src/script.js
+++ src/script.js
@@ -1,64 +1,57 @@
-const shoes = [
+const shoesData = [
   {
     name: "TY",
     src: `./images/1-range.png`,
-    id: 1,
   },
   {
     name: "MAX",
     src: `./images/2-range.png`,
-    id: 2,
   },
   {
-    name: "EMME",
+    name: "EMMIE",
     src: `./images/3-range.png`,
-    id: 3,
   },
   {
     name: "CAMILLE",
     src: `./images/4-range.png`,
-    id: 4,
   },
   {
     name: "NOEL",
     src: `./images/5-range.png`,
-    id: 5,
   },
   {
     name: "ROAN",
     src: `./images/6-range.png`,
-    id: 6,
   },
   {
     name: "NAT",
     src: `./images/7-range.png`,
-    id: 7,
   },
 ];
-let id = 1;
+let index = 0;
 
-const rangeItem = document.getElementById("range");
-const modelName = document.getElementById("model-name");
-const modelNumber = document.getElementById("model-number");
+const rangeItem = document.getElementById("slider");
+const modelName = document.getElementById("shoes-name");
+const modelNumber = document.getElementById("shoes-number");
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
 
 const image = document.createElement("img");
 rangeItem.appendChild(image);
 
-function range(id) {
-  const model = shoes.find((model) => model.id === id);
-  image.src = model.src;
-  modelName.innerText = model.name;
-  modelNumber.innerText = `${model.id}/${shoes.length}`;
+function range(index) {
+  const shoes = shoesData[index];
+  image.src = shoes.src;
+  modelName.innerText = shoes.name;
+  modelNumber.innerText = `${index + 1}/${shoesData.length}`;
 }
-range(id);
+range(index);
 
 prevBtn.addEventListener("click", function () {
-  id !== 1 ? id-- : (id = 7);
-  range(id);
+  index === 0 ? (index = shoesData.length - 1) : index--;
+  range(index);
 });
 nextBtn.addEventListener("click", function () {
-  id !== 7 ? id++ : (id = 1);
-  range(id);
+  index === (shoesData.length - 1) ? (index = 0) : index++;
+  range(index);
 });
